b62c2a6f530e0eb141acdedc73a6062f94bbb9c0
Victor Bittencourt <victorgcb@outlook.com>
2025-07-07 21:49:36 -0300

    STP-FEAT: Fixing Homepage

--- views/0_homepage.py
+++ views/0_homepage.py
@@ -2,18 +2,35 @@ import streamlit as st
 from streamlit_extras.stylable_container import stylable_container
 
 def main():
-    st.title("Streamlit Main Example")
+    st.title("Página Inicial")
+    st.markdown("""
+        Bem-vindo ao meu aplicativo simples do Streamlit, aqui irei exibir algumas
+        interações simples entre várias tecnologias como Pandas, Typing, Streamlit e outros!
+        Iremos aproveitar e mexer um pouco com tratamento e análise de dados também!
+    """)
 
-    with stylable_container(
-        css_styles="""{
-            "backgroundColor": "#f0f0f0",
-            "padding": "20px",
-            "borderRadius": "10px",
-            "boxShadow": "0 2px 4px rgba(0, 0, 0, 0.1)"
-        }""",
-        key="custom_container"
-    ):
-        st.write("This is a custom styled container.")
-        st.button("Click Me")
+    st.divider()
+
+    st.subheader("Sobre o Projeto")
+    st.markdown("""
+        Este projeto é um exemplo simples de como usar o Streamlit para criar uma aplicação
+        web interativa. Ele demonstra como carregar e exibir dados usando o Pandas, além
+        de como estilizar componentes com o Streamlit Extras.
+    """)
+
+    st.subheader("Tecnologias Utilizadas")
+    st.markdown("""
+        - **Streamlit**: Para criar a interface web interativa.
+        - **Pandas**: Para manipulação e análise de dados.
+        - **Streamlit Extras**: Para componentes adicionais e estilização.
+    """)
+
+    st.divider()
+    st.subheader("Redes Sociais")
+    st.markdown("""
+        Você pode me encontrar nas seguintes redes sociais:
+        - [GitHub](https://github.com/VicourtBitt)
+        - [LinkedIn](https://www.linkedin.com/in/victorbitt/)
+    """)
     
 main()          
